539ad68fbc164f24058ae2a7c49eb2f10f89f161
mike2505 <37780087+mike2505@users.noreply.github.com>
2022-01-31 18:39:22 +0400

    Create blocks.def.h

---
+++ .config/drwmblocks/blocks.def.h
@@ -0,0 +1,12 @@
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+    {" ", "/opt/dwmblocks-distrotube-git/scripts/upt",		    360,		        2},
+	
+	{" ", "/opt/dwmblocks-distrotube-git/scripts/memory",	        60,		            2},
+
+	{" ", "/opt/dwmblocks-distrotube-git/scripts/volume",			2,		            10},
+
+	{" ", "/opt/dwmblocks-distrotube-git/scripts/clock",			5,		            0},
+};
+
+static char delim = '|';
